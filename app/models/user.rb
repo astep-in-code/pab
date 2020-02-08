c32311fcd288e31fc528a9b8d3fbc1e195231a5e
Stephane Monin <steph.monin+github@gmail.com>
2020-02-08 10:46:21 +0100

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  # validates :birthday, presence: true
+  validates :date_of_birth, presence: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
