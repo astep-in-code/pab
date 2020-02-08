9c7c228e23c5e225b5f4b08875b6890d497f5a7b
Stephane Monin <steph.monin+github@gmail.com>
2020-02-08 17:15:55 +0100

    First seed plus ou moins OK

--- app/models/beer_sub_step.rb
+++ app/models/beer_sub_step.rb
@@ -1,5 +1,5 @@
 class BeerSubStep < ApplicationRecord
-  belongs_to :beer_steps
-  belongs_to :brew_steps, through: :beer_steps
+  belongs_to :beer_step
+  # belongs_to :brew_steps, through: :beer_steps
   belongs_to :beer
 end
