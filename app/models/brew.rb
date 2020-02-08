b5e03cf428dcee09b76399edece9c113ac85469f
Stephane Monin <steph.monin+github@gmail.com>
2020-02-08 10:29:32 +0100

    seed version non fonctionnel en attente de models

--- app/models/brew.rb
+++ app/models/brew.rb
@@ -1,8 +1,8 @@
 class Brew < ApplicationRecord
   belongs_to :beer
   belongs_to :user
-  belongs_to :ispindle
-  belongs_to :brew_step
+#  belongs_to :ispindle
+#  belongs_to :brew_step
 
   has_many :brew_steps, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Beer.destroy_all
-<<<<<<< HEAD
 Brew.destroy_all
 User.destroy_all
-Beer_step.destroy_all
+BeerStep.destroy_all
 
 myUser = User.create({
   email: "contact@pretabrasser.com",
@@ -18,7 +17,6 @@ myUser = User.create({
 })
 
 myBeer = Beer.new({
-myBeer = ({
   name: "Smash 66",
   style: "Smash",
   volume: 23,
@@ -59,10 +57,58 @@ myBeer = ({
   }
 })
 
-p "Model valide et enregistrable" if myBeer.valid?
 p "Biere sauvegardee" if myBeer.save
 
 myBrew = Brew.new({
   beer_id: Beer.last.id,
   user_id: User.last.id
 })
+
+# p myBrew.valid?
+p "My Brew saved" if myBrew.save
+
+myBrewStep = BrewStep.new({
+  status: 0,
+  brew_id: Brew.last.id,
+})
+
+p myBrewStep.valid?
+
+myBeerStep = BeerStep.new({
+  step: 0,
+  description:"Préparation de la recette"
+})
+
+myBeerStep.valid?
+
+myBeerSubStep = BeerSubStep.new({
+  id_beer_steps: 0,
+  id_beer: Beer.last.id,
+  "substep":{
+    "0": {
+      "title": "Matériel dont vous aurez besoin pour cette étape:",
+      "content": "Vous aurez besoin de vous munir de votre produit de rinçage. Dans le kit vous est fourni un flacon d'Oxy. C'est une solution à base d'oxygène actif qui va permettre de désinfecter votre matériel qui sera en contact avec le mou afin d'éviter tout risque d'infection. Vous aurez également besoin de votre seau de fermentation, d'un récipient de 7 litre minimum, de votre ispindle, de votre syphon et de votre barboteur.",
+      "time": "0"
+    },
+    "1": {
+      "title": "Nettoyage des éléments de brassage",
+      "content": "Munissez-vous d'un seau et remplissez-le d'eau. Lorsque celui-ci est plein, mélangez votre produit de rinçage dans l'eau et mélangez bien. Prenez ensuite l'ensemble des ustensilles que vous avez précédement réuni et plongez-les dans l'eau. L'oxygen actif que vous utilisez lors de cette étape de brassage doit agir durant une durée d'au moins 10 minutes. Pendant que le produit agis, vous pouvez passer à l'étape suivante.",
+      "time": 900
+    },
+    "2": {
+      "title": "Préparation de l'eau de rinçage",
+      "content": "Munissez-vous d'un seau et placez-y 7l d'eau. Cette eau devra être portée à une température de 82°c lors de votre rinçage et permettra d'extraire un maximum de sure du mout.",
+      "time": 0
+    },
+    "3": {
+      "title": "Préchauffe de l'eau de brassage",
+      "content": "Préchauffer les 15l d'eau de brassage dont vous aurez besoin à uen températur de 69°c. La température est légèrement plus chaude que la température cible d'empatage car le grain va refroidir votre volume d'eau en y étant mélangé. Pendant que l'eau chauffe, munissez-vous de votre matériel de brassage dans l'étape suivante",
+      "time": "900"
+    },
+    "4": {
+      "title": "Préparation du matériel de brassage",
+      "content": "Pendant que votre eau préchauffe, munissez vous de : Votre grain concassé, votre houblon, votre spatule de brassage et votre sac de brassage."
+    }
+  }
+})
+ p myBeerSubStep.valid?
