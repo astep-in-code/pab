01df4bc27aa0bb81fb43ee490ffcce482e409b79
Stephane Monin <steph.monin+github@gmail.com>
2020-02-11 19:38:01 +0100

    quick fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -106,32 +106,32 @@ myBeerSubStep = BeerSubStep.new({
   beer_step_id: myBeerStep.id,
   beer_id: myBeer.id,
   sub_step: '{'\
-    '"0": {'\
-      '"title": "Matériel dont vous aurez besoin pour cette étape:",'\
-      '"content": "Vous aurez besoin de vous munir de votre produit de rinçage. Dans le kit vous est fourni un flacon d'Oxy. C'est une solution à base d\'oxygène actif qui va permettre de désinfecter votre matériel qui sera en contact avec le mou afin d\'éviter tout risque d\'infection. Vous aurez également besoin de votre seau de fermentation, d\'un récipient de 7 litre minimum, de votre ispindle, de votre syphon et de votre barboteur.",'
-      '"time": "0"'\
-    '},'\
-    '"1": {'\
-      '"title": "Nettoyage des éléments de brassage",'\
-      '"content": "Munissez-vous d\'un seau et remplissez-le d\'eau. Lorsque celui-ci est plein, mélangez votre produit de rinçage dans l\'eau et mélangez bien. Prenez ensuite l\'ensemble des ustensilles que vous avez précédement réuni et plongez-les dans l\'eau. L\'oxygen actif que vous utilisez lors de cette étape de brassage doit agir durant une durée d\'au moins 10 minutes. Pendant que le produit agis, vous pouvez passer à l\'étape suivante.",'
-      '"time": 900'\
-    '},'\
-    '"2": {'\
-      '"title": "Préparation de l\'eau de rinçage",'\
-      '"content": "Munissez-vous d\'un seau et placez-y 7l d\'eau. Cette eau devra être portée à une température de 82°c lors de votre rinçage et permettra d\'extraire un maximum de sure du mout.",'\
-      '"time": 0'\
-    '},'\
-    '"3": {'\
-      '"title": "Préchauffe de l\'eau de brassage",'\
-      '"content": "Préchauffer les 15l d\'eau de brassage dont vous aurez besoin à uen températur de 69°c. La température est légèrement plus chaude que la température cible d\'empatage car le grain va refroidir votre volume d\'eau en y étant mélangé. Pendant que l\'eau chauffe, munissez-vous de votre matériel de brassage dans l\'étape suivante",'\
-      '"time": "900"'\
-    '},'\
-    '"4": {'\
-      '"title": "Préparation du matériel de brassage",'\
-      '"content": "Pendant que votre eau préchauffe, munissez vous de : Votre grain concassé, votre houblon, votre spatule de brassage et votre sac de brassage.",'\
-      '"time": "0"'
-    '}'\
-  '}'
+  #   '"0": {'\
+  #     '"title": "Matériel dont vous aurez besoin pour cette étape:",'\
+  #     '"content": "Vous aurez besoin de vous munir de votre produit de rinçage. Dans le kit vous est fourni un flacon d'Oxy. C'est une solution à base d\'oxygène actif qui va permettre de désinfecter votre matériel qui sera en contact avec le mou afin d\'éviter tout risque d\'infection. Vous aurez également besoin de votre seau de fermentation, d\'un récipient de 7 litre minimum, de votre ispindle, de votre syphon et de votre barboteur.",'
+  #     '"time": "0"'\
+  #   '},'\
+  #   '"1": {'\
+  #     '"title": "Nettoyage des éléments de brassage",'\
+  #     '"content": "Munissez-vous d\'un seau et remplissez-le d\'eau. Lorsque celui-ci est plein, mélangez votre produit de rinçage dans l\'eau et mélangez bien. Prenez ensuite l\'ensemble des ustensilles que vous avez précédement réuni et plongez-les dans l\'eau. L\'oxygen actif que vous utilisez lors de cette étape de brassage doit agir durant une durée d\'au moins 10 minutes. Pendant que le produit agis, vous pouvez passer à l\'étape suivante.",'
+  #     '"time": 900'\
+  #   '},'\
+  #   '"2": {'\
+  #     '"title": "Préparation de l\'eau de rinçage",'\
+  #     '"content": "Munissez-vous d\'un seau et placez-y 7l d\'eau. Cette eau devra être portée à une température de 82°c lors de votre rinçage et permettra d\'extraire un maximum de sure du mout.",'\
+  #     '"time": 0'\
+  #   '},'\
+  #   '"3": {'\
+  #     '"title": "Préchauffe de l\'eau de brassage",'\
+  #     '"content": "Préchauffer les 15l d\'eau de brassage dont vous aurez besoin à uen températur de 69°c. La température est légèrement plus chaude que la température cible d\'empatage car le grain va refroidir votre volume d\'eau en y étant mélangé. Pendant que l\'eau chauffe, munissez-vous de votre matériel de brassage dans l\'étape suivante",'\
+  #     '"time": "900"'\
+  #   '},'\
+  #   '"4": {'\
+  #     '"title": "Préparation du matériel de brassage",'\
+  #     '"content": "Pendant que votre eau préchauffe, munissez vous de : Votre grain concassé, votre houblon, votre spatule de brassage et votre sac de brassage.",'\
+  #     '"time": "0"'
+  #   '}'\
+  # '}'
 })
  p myBeerSubStep.valid?
  myBeerSubStep.save!
