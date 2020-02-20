5d04c11608d7c5b92f36530b9f23718fd12bb4ea
Hanna Nabet <hanna.nabet@yahoo.fr>
2020-02-20 20:05:22 +0100

    update BrewStep def methode

--- app/controllers/brew_steps_controller.rb
+++ app/controllers/brew_steps_controller.rb
@@ -25,6 +25,18 @@ class BrewStepsController < ApplicationController
       @beer_sub_step = BeerSubStep.where(beer_id: @brew.beer.id)
     end
 
+    def update_step
+      @brew = Brew.find(params[:id])
+      @brew_step = BrewStep.where(brew_id: @brew.id)
+      status = @brew_step[0].status += 1
+      @brew_step[0].update(status: status)
+      step = @brew_step[0].beer_step_id +=1
+      @brew_step[0].update(beer_step_id: step)
+      # @brew_step.save!
+
+      redirect_to brew_path(@brew)
+    end
+
     def update
       respond_to do |format|
         if @brewstep.update(brewstep_params)
